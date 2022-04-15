7c2549e6c35aa9f980065368f048eb7e9bb5f65c
Jordi <j.z.cuffi@gmail.com>
2022-04-15 09:58:52 +0200

    "removed comments"

--- problema1.py
+++ problema1.py
@@ -1,47 +1,27 @@
 # ---------------------------- Problema1 1 --------------------------
 
 def es_multiplo(numero, multiplo):
-    """
-    Devuelve True o False dependiendo de si es multiplo o no
-    
-    input:
-    numero -> el numero que queremos evaluar
-    multiplo -> el multiplo
-    
-    output:
-    devuelve un booleano con la respuesta
-    """
     return not bool(numero % multiplo)
 
 
-def problema1():      
-    """
-    Esta función devuelve el resultado del problema 1
-    """
-    
+def problema1():
     cadena = list(range(1, 100))
-    
     cadena_nueva = []
 
     for numero in cadena:
-        
         multiplo_de_tres = es_multiplo(numero, 3)
         multiplo_de_cinco = es_multiplo(numero, 5)
         
         if multiplo_de_tres and multiplo_de_cinco:
-
             cadena_nueva.append("cyberclick")
 
         elif multiplo_de_tres:
-
             cadena_nueva.append("cyber")
 
-        elif multiplo_de_cinco:  
-
+        elif multiplo_de_cinco:
             cadena_nueva.append("click")        
 
         else:
-
             cadena_nueva.append(numero)
             
     return cadena_nueva
--- problema2_1.py
+++ problema2_1.py
@@ -1,25 +1,15 @@
 # ---------------------------- Problema1 2: Parte 1 --------------------------
 
 def test1():
-    """
-    prueba que el resultado funciona para los ejemplos dados
-    True -> Pasa el test
-    """
     textos = ['1-3 a: abcde', '1-3 b: cdefg', '2-9 c: ccccccccc']
     validez = [True, False, True]
-
     resultado = validez_politica_lista(textos)
-
     return resultado == validez
 
 
 class Texto_input:
-    def __init__(self,texto):
-        """
-        divide el texto en min,max,letra y contraseña
-        """
+    def __init__(self, texto):
         lista = texto.split(" ")
-
         numeros = lista[0].split("-")
         self.primer_numero = int(numeros[0])
         self.segundo_numero = int(numeros[1])
@@ -28,9 +18,6 @@ class Texto_input:
 
 
 def validez_contrasena(texto_input):
-    """
-    comprueba si se cumple la política de repetición de una letra entre un minimo y un maximo numero de veces
-    """
     minimo = texto_input.primer_numero
     maximo = texto_input.segundo_numero
     contrasena = texto_input.contrasena
@@ -39,23 +26,14 @@ def validez_contrasena(texto_input):
 
 
 def formato_correcto(texto):
-    """
-    Hace algunas comprobaciones basicas del input
-    """
     return texto and texto[0].isnumeric and texto[-1].isalpha() and ("-" in texto) and (":" in texto)
 
 
 def validez_politica(texto):
-    """
-    comprueba la validez de la contraseña respecto a la politica
-    """
     return validez_contrasena(Texto_input(texto)) if formato_correcto(texto) else False
 
 
 def validez_politica_lista(lista_texto):
-    """
-    Nos devuelve una lista con los resultados True o False de la validez de la contraseña según la política
-    """
     return [validez_politica(texto) for texto in lista_texto]
 
 
@@ -67,7 +45,4 @@ def cargar_lista():
 
 
 def problema2():
-    """
-    Función de resultado del problema 2 parte 1
-    """
     return sum(validez_politica_lista(cargar_lista()))
